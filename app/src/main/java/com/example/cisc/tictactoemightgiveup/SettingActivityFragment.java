2a98de553526e8204d95b53ee239aaef0d7cf4a8
CISC Student <cisc@ac.sdmesa.net>
2017-10-26 12:30:47 -0700

    Initial commit

---
+++ app/src/main/java/com/example/cisc/tictactoemightgiveup/SettingActivityFragment.java
@@ -0,0 +1,22 @@
+package com.example.cisc.tictactoemightgiveup;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class SettingActivityFragment extends Fragment {
+
+    public SettingActivityFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        addPreferenceFromResource(R.xml.)
+    }
+}
